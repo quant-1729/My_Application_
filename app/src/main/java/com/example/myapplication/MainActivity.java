f1b11ecfd8837bc602ed78e5ed974c3d626443f1
quant-1729 <priyanshuagrawal1729@gmail.com>
2023-07-23 23:09:19 +0530

    this is second commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.myapplication.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView textView= findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1= new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
